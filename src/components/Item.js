edb3a932b37c4cfa24a5d0459c35f4a230177a2d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-30 20:50:03 +0700

    Check selected item

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,11 @@ export default class Item extends React.Component {
   render() {
     let { item } = this.props
     let { chooseItem } = this.props
-    return <div onClick={e => chooseItem(item.id)}>{item.display_name}</div>
+    let { isSelected } = this.props
+
+    let className = ""
+    className = isSelected(item.id) ? className + " selected" : className
+
+    return <div className={className} onClick={e => chooseItem(item.id)}>{item.display_name}</div>
   }
 }
--- src/containers/HoiItem.js
+++ src/containers/HoiItem.js
@@ -2,9 +2,15 @@ import { connect } from "react-redux"
 import Item from "../components/Item"
 import { actionChooseItem } from "../actions"
 
-const mapStateToProps = null
+const mapStateToProps = ({ order }) => {
+  let { item_id: orderItemId } = order
+
+  return {
+    isSelected: item_id => item_id === orderItemId
+  }
+}
 const mapActionToProps = dispatch => ({
   chooseItem: item_id => dispatch(actionChooseItem(item_id))
 })
 
-export default connect(null, mapActionToProps)(Item)
+export default connect(mapStateToProps, mapActionToProps)(Item)
