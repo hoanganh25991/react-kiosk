7ca8e573997032c835f6c1c2e36951d52fada57d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-04 07:27:50 +0700

    Rename selector function

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,16 +1,26 @@
 import { createSelector } from "reselect"
 
+// From table data
 const categories = state => state.categories
+const items = state => state.items
+const modifiers = state => state.modifier_groups
+// From pivot
+const pivotCategoryItems = state => state.category_items
+const pivotItemModifiers = state => state.item_modifier_groups
+const pivotModifierItems = state => state.modifier_group_items
+// Form order
 const orderCategoryId = state => state.order.category_id
+const orderItemId = state => state.order.item_id
+
+// Get subcategories from order category id
 export const getSubCategoriesByCategory = createSelector([categories, orderCategoryId], (categories, category_id) =>
   categories.filter(category => category.main_category_id === category_id)
 )
 
-const categoryId = (state, props) => props.category_id
-const pivotCategoryItems = state => state.category_items
-const items = state => state.items
+const categoryIdFromProps = (state, props) => props.category_id
+
 export const getItemsByCategory = createSelector(
-  [categoryId, pivotCategoryItems, items],
+  [categoryIdFromProps, pivotCategoryItems, items],
   (categoryId, pivotCategoryItems, items) => {
     let item_ids = pivotCategoryItems.filter(pivot => pivot.category_id === categoryId).map(pivot => pivot.item_id)
     return items.filter(item => item_ids.includes(item.id))
