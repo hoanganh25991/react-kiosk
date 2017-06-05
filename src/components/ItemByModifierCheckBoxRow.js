20c43682cc9335d7c4e1b7556e39765f265ebd6a
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-05 15:31:34 +0700

    StopPropagation event not work, trick by separate click event

--- src/components/ItemByModifierCheckBoxRow.js
+++ src/components/ItemByModifierCheckBoxRow.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default class ItemByModifierCheckboxRow extends React.Component {
   removeItemByModifier = e => {
-    e.preventDefault()
+    e.stopPropagation()
+    console.log(e.target)
+    console.log("removeItemByModifier")
     let { item } = this.props
     let { modifier } = this.props
     let { actionAddItemByModifierToBagTemporary } = this.props
@@ -15,11 +17,18 @@ export default class ItemByModifierCheckboxRow extends React.Component {
     let { itemByModifierQuantityTemporary } = this.props
     let { actionAddItemByModifierToBagTemporary } = this.props
     return (
-      <div className="padding10" onClick={e => actionAddItemByModifierToBagTemporary(modifier.id, item.id, 1)}>
+      <div className="flexRow padding10">
         {[...Array(itemByModifierQuantityTemporary).keys()].map(key => (
           <input key={key} type="checkbox" checked={true} onChange={e => this.removeItemByModifier(e)} />
         ))}
         <span>{item.display_name}</span>
+        <div
+          className="flex1 noBorder"
+          onClick={e => {
+            console.log(e.target)
+            actionAddItemByModifierToBagTemporary(modifier.id, item.id, 1)
+          }}
+        />
       </div>
     )
   }
